1050416bf3863a317225131dcef72af3af5080ca
fueledbyflux <fueledbyflux@gmail.com>
2015-02-22 21:02:13 +1100

    revert

--- FuckingAwesomeDraven/FuckingAwesomeDraven/AxeCatcher.cs
+++ FuckingAwesomeDraven/FuckingAwesomeDraven/AxeCatcher.cs
@@ -298,7 +298,6 @@ namespace FuckingAwesomeDraven
 
         private static bool ShouldWait()
         {
-            Game.PrintChat(Program.Config.Item("FarmDelay").GetValue<Slider>().Value.ToString());
             return
                 ObjectManager.Get<Obj_AI_Minion>()
                     .Any(
