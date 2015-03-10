be4bb0d3ae0d14b23a318050e9000f410c664947
fueledbyflux <fueledbyflux@gmail.com>
2015-03-10 20:43:31 +1100

    ifag added Kappa

---
+++ iFuckingAwesomeGraves/iFuckingAwesomeGraves/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace iFuckingAwesomeGraves
+{
+    class Program
+    {
+
+        public static Orbwalking.Orbwalker Orbwalker;
+        public static Menu Config;
+
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        static void Game_OnGameLoad(EventArgs args)
+        {
+            Notifications.AddNotification(new Notification("THIS IS NOT DONE", 2));
+
+            Config = new Menu("iFuckingAwesomeGraves", "ifag", true);
+
+            Orbwalker = new Orbwalking.Orbwalker(Config.AddSubMenu(new Menu("Orbwalker", "Orbwalker")));
+            
+            TargetSelector.AddToMenu(Config.AddSubMenu(new Menu("Target Selector", "Target Selector")));
+            
+            Config.AddToMainMenu();
+        }
+    }
+}
