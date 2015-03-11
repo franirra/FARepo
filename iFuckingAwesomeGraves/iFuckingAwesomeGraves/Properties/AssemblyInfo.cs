0fd12132fd99f7096ab60154d2b073b3859c845f
fueledbyflux <fueledbyflux@gmail.com>
2015-03-11 17:58:32 +1100

    kek

--- iFuckingAwesomeGraves/iFuckingAwesomeGraves/Properties/AssemblyInfo.cs
+++ iFuckingAwesomeGraves/iFuckingAwesomeGraves/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("iFuckingAwesomeGraves")]
+[assembly: AssemblyTitle("iFuckingAwesomeGraves - NOT DONE!")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("iFuckingAwesomeGraves")]
+[assembly: AssemblyProduct("iFuckingAwesomeGraves - NOT DONE!")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
